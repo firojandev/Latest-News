c1940cdd0c8a1fb8df8f6373184c27b7ca8c2562
altaf <altaf@squaregroup.com>
2022-07-06 14:52:33 +0600

    Initial Commit

---
+++ Latest News/Latest_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Latest_NewsApp.swift
+//  Latest News
+//
+//  Created by Md Altaf Hoshain Firoj on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Latest_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
